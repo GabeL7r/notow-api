4bd1176ad13d077e74995ddeeff22f55b23ca63c
Mariia Rudenko <rudenkom@mrudenko-mbpr.local>
2018-03-24 13:40:35 -0700

    added base64 decoding

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from chalice import Chalice
 import boto3
+import base64
 
 app = Chalice(app_name='reactathon-api')
 
@@ -14,7 +15,7 @@ def detect_text_handler(image, context):
 
     rekognition = boto3.client("rekognition", "us-west-2")
     response = rekognition.detect_text(Image={
-        "Bytes": image.encode()
+        "Bytes": base64.decodestring(image)
     })
 
     return {
