fd2d4be97f25ddb83bb8001036805118e0bd7cb3
nrebhun <nick@nrebhun.com>
2018-03-24 13:32:13 -0700

    Initial text detection

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from chalice import Chalice
+import boto3
 
 app = Chalice(app_name='reactathon-api')
 
@@ -8,22 +9,14 @@ def index():
     return {'hello': 'world'}
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/detect')
+def detect_text_handler(image, context):
+
+    rekognition = boto3.client("rekognition", "us-west-2")
+    response = rekognition.detect_text(Image={
+        "Bytes": image.encode()
+    })
+
+    return {
+        'message' : response
+    }
