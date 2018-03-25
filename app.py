436e3291bace37e55a6e2545f7107d47ba227138
nrebhun <nick@nrebhun.com>
2018-03-24 17:07:05 -0700

    tweak app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def parse_handler():
     data = []
     if request.method == 'POST' and 'message' in request.json_body:
         data = request.json_body['message']
-        parse()
+        parse_sign()
     else:
         return Response(
                     body={"error": "Message not provided!"},
@@ -25,21 +25,23 @@ def parse_handler():
     return data
 
 # parse() returns an array of valid parking times each day
-def parse(text = 'NO PARKING ANY TIME'):
+def parse_sign(text = 'NO PARKING ANY TIME'):
     numeric_map = ['ZERO', 'ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE']
     common_days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
     common_minutes = [1, 2, 5, 10, 15, 20, 30]
     time_delimeters = ['MINUTE', 'HOUR']
 
+    time_range_regex = re.compile(r'^\d{1,2}[aA|pP][mM]-\d{0,2}[aA|pP][mM]$')
     alt_text = 'ONE HOUR PARKING 9AM-7PM'
 
     split_text = text.split('PARKING')
     time_limit = split_text[0]
-    when = split_text[1]
+    when_rule_is_valid = split_text[1]
 
-    if 'NO' in split_text[0]:
-
-
+    if time_limit is 'NO':
+        # Figure out when this rule applies...
+        if time_range_regex.search(when_rule_is_valid):
+            
 
 
     # case for number followed by delimeter (2 hour parking, 30 minute parking)
